1b319071e863ada5e74f3da20ee4e95fd66a1ce9
JoeBashaar <155857418+JoeBashaar@users.noreply.github.com>
2024-12-16 00:12:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
-from pizza import (
-    PizzaFactory,
-    CheeseTopping,
-    OlivesTopping,
-    MushroomsTopping,
-    Order,
-    PayPalPayment,
-    CreditCardPayment,
-    InventoryManager
-)
+from abc import ABC, abstractmethod
+from pizza import PizzaFactory, CheeseTopping, OlivesTopping, MushroomsTopping
+from inventory import InventoryManager
 
-def main(): #Example usage
+def main():
+    inventory_manager = InventoryManager()
     pizza_factory = PizzaFactory()
-    pizza = pizza_factory.create_pizza("margherita")
-    pizza = CheeseTopping(pizza)
-    pizza = OlivesTopping(pizza)
-    print(f"Pizza: {pizza.get_description()}")
-    print(f"Total Cost: ${pizza.get_cost():.2f}")
-    order = Order(PayPalPayment())
-    if order.process_order(pizza):
-        print("Payment successful!")
+
+    print("Welcome to the Pizza Restaurant!")
+
+    while True:
+        print("\nChoose your base pizza:")
+        print("1. Margherita ($5.0)")
+        print("2. Pepperoni ($6.0)")
+        print("0 => to exit")
+        pizza_choice = input("Enter the number of your choice: ")
+        
+        if pizza_choice == '0':
+            break
+
+        pizza = pizza_factory.create_pizza(pizza_choice, inventory_manager)
+        if not pizza:
+            print("Pizza unavailable or out of stock!")
+            continue
+
+        while True:
+            print("\nAvailable toppings:")
+            print("1. Cheese ($1.0)")
+            print("2. Olives ($0.5)")
+            print("3. Mushrooms ($0.7)")
+            print("4. Finish order")
+            topping_choice = input("Enter the number of your choice: ")
+
+            if topping_choice == "1" and inventory_manager.check_and_decrement("Cheese"):
+                pizza = CheeseTopping(pizza)
+            elif topping_choice == "2" and inventory_manager.check_and_decrement("Olives"):
+                pizza = OlivesTopping(pizza)
+            elif topping_choice == "3" and inventory_manager.check_and_decrement("Mushrooms"):
+                pizza = MushroomsTopping(pizza)
+            elif topping_choice == "4":
+                break
+            else:
+                print("Topping unavailable or out of stock!")
+
+        print("\nYour order:")
+        print(f"Description: {pizza.get_description()}")
+        print(f"Total cost: ${pizza.get_cost():.2f}")
+
+        print("\nRemaining Inventory:")
+        print(inventory_manager.get_inventory())
 
 if __name__ == "__main__":
     main()
